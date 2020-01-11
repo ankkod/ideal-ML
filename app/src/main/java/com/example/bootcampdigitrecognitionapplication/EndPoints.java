74e4f775d2f769e0964c0f452bfdd3bc078db948
Ankit Kodalikar <ankitkodalikar94@gmail.com>
2020-01-11 15:10:11 +0530

    all files

---
+++ app/src/main/java/com/example/bootcampdigitrecognitionapplication/EndPoints.java
@@ -0,0 +1,5 @@
+package com.example.bootcampdigitrecognitionapplication;
+
+public class EndPoints {
+    public static final String PREDICT_URL = "http://ec2-3-225-19-65.compute-1.amazonaws.com/model/predict";
+}
